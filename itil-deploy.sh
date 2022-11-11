8825dbad53badcfe1d8034be89e372ac55fd5e2d
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2022-11-11 21:14:35 +0700

    Update itil-deploy.sh
    Signed-off-by: PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>

--- itil-deploy.sh
+++ itil-deploy.sh
@@ -362,6 +362,14 @@ sudo chown www-data /var/www/html/$FOLDERDATA
 #Step 7: Finish GLPI installation
 cat > /etc/hosts <<END
 127.0.0.1 $FQDN
+127.0.0.1 localhost
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
 END
 
 #Visit your server IP or hostname URL on /glpi. If it is your local machine, you can use: http://127.0.0.1/glpi/install/install.php
