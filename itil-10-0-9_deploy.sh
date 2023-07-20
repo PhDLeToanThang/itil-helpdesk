60a8daa4598b4389beede5d7467086b64c0def08
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2023-07-20 22:16:26 +0700

    Update itil-10-0-9_deploy.sh
    Signed-off-by: PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>

--- itil-10-0-9_deploy.sh
+++ itil-10-0-9_deploy.sh
@@ -1,26 +1,24 @@
-# Step 1: Update Ubuntu
-# sudo apt update
-# sudo apt -y upgrade
-
+#!/bin/bash
 clear
 cd ~
+
 ############### Tham số cần thay đổi ở đây ###################
 echo "FQDN: e.g: demo.company.vn"   # Đổi địa chỉ web thứ nhất Website Master for Resource code - để tạo cùng 1 Source code duy nhất 
-read -e FQDN
+read -p FQDN
 echo "dbname: e.g: itildata"   # Tên DBNane
-read -e dbname
+read -p dbname
 echo "dbuser: e.g: userdata"   # Tên User access DB lmsatcuser
-read -e dbuser
+read -p dbuser
 echo "Database Password: e.g: P@$$w0rd-1.22"
 read -s dbpass
 echo "phpmyadmin folder name: e.g: phpmyadmin"   # Đổi tên thư mục phpmyadmin khi add link symbol vào Website 
-read -e phpmyadmin
+read -p phpmyadmin
 echo "ITIL Folder Data: e.g: itildata"   # Tên Thư mục chưa Data vs Cache
-read -e FOLDERDATA
+read -p FOLDERDATA
 echo "dbtype name: e.g: mariadb"   # Tên kiểu Database
-read -e dbtype
+read -p dbtype
 echo "dbhost name: e.g: localhost"   # Tên Db host connector
-read -e dbhost
+read -p dbhost
 
 GitGLPIversion="10.0.9"
 
