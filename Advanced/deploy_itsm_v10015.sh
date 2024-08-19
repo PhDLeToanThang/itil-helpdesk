f998a4a62ac05fcab526647fb10bb626ce357411
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2024-08-19 13:21:45 +0700

    Update deploy_itsm_v10015.sh
    Signed-off-by: PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>

--- Advanced/deploy_itsm_v10015.sh
+++ Advanced/deploy_itsm_v10015.sh
@@ -65,7 +65,7 @@ sudo mysql_secure_installation
 
 #Step 3. Install PHP-FPM & Related modules
 sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
+sudo -S add-apt-repository ppa:ondrej/php -y
 sudo apt update -y
 sudo apt install php8.0-fpm php8.0-common php8.0-mbstring php8.0-xmlrpc php8.0-soap php8.0-gd php8.0-xml php8.0-intl php8.0-mysql php8.0-cli php8.0-mcrypt php8.0-ldap php8.0-zip php8.0-curl php8.0-bz2 -y
 
